a3ecf729663ae7960fcd321448b2c53e62ad2de7
Ligita <legyta@gmail.com>
2021-01-12 13:13:42 +0100

    env settings to heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 require("dotenv").config();
 
 // set up express
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const buildPath = path.join(__dirname, '../client/', 'build');
+const buildPath = path.join(__dirname, "../client/", "build");
 app.use(express.static(buildPath));
 
 const PORT = process.env.PORT || 5000;
@@ -18,6 +18,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
 // set up mongoose
+const aws = require("aws-sdk");
+let S3_KEY = new aws.S3({
+  MONGODB_CONNECTION_STRING: process.env.S3_KEY,
+});
 
 mongoose.connect(
   process.env.MONGODB_CONNECTION_STRING,
