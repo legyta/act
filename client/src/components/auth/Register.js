c6e46e0bcb1f066a24bd947e6a5a6fc0bf207e81
Scott Green <scott_green_1+github@pm.me>
2021-01-30 18:42:58 +0100

    parse register errors on front end

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -13,88 +13,102 @@ import register from "../styling/register.css";
 const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export default function Register() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [passwordCheck, setPasswordCheck] = useState();
-  const [displayName, setDisplayName] = useState();
-  const [error, setError] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [passwordCheck, setPasswordCheck] = useState();
+    const [displayName, setDisplayName] = useState();
+    const [error, setError] = useState();
 
-  const { setUserData } = useContext(UserContext);
-  const history = useHistory();
+    const { setUserData } = useContext(UserContext);
+    const history = useHistory();
 
-  const submit = async (e) => {
-    e.preventDefault();
+    const submit = async (e) => {
+        e.preventDefault();
 
-    try {
-      const newUser = { email, password, passwordCheck, displayName };
-      await Axios.post(`${BASE_API_URL}/users/register`, newUser);
-      const loginResponse = await Axios.post(`${BASE_API_URL}/users/login`, {
-        email,
-        password,
-      });
-      setUserData({
-        token: loginResponse.data.token,
-        user: loginResponse.data.user,
-      });
-      localStorage.setItem("auth-token", loginResponse.data.token);
-      history.push("/profile");
-    } catch (err) {
-      // err.response.data.msg && setError(err.response.data.msg);
-      console.log(err);
-    }
-  };
+        // try {
+            const newUser = { email, password, passwordCheck, displayName };
+            await Axios.post(`${BASE_API_URL}/users/register`, newUser)
+                .then(async function (response) {
+                    // TODO put login in a .then(), .cath() function
+                    const loginResponse = await Axios.post(`${BASE_API_URL}/users/login`, {
+                        email,
+                        password,
+                    });
+                    setUserData({
+                        token: loginResponse.data.token,
+                        user: loginResponse.data.user,
+                    });
+                    localStorage.setItem("auth-token", loginResponse.data.token);
+                    history.push("/profile");
+                })
+                .catch(function (error) {
+                    //  TODO display errors in site
+                    if (error.response.status === 422){
+                        error.response?.data?.errors.forEach(element => {
+                            console.log(`${element?.param} failed with message: ${element?.msg}`)
+                        });
+                    }
+                    else {
+                        console.log(error.response)
+                    }
+                })
+        // } catch (err) {
+        //     // err.response.data.msg && setError(err.response.data.msg);
+        //     console.log(err.response);
+        // }
+    };
 
-  return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="left"
-      className="page"
-    >
-      <Header />
-      <Box className="register-page">
-        <h1>Register</h1>
-        {error && (
-          <ErrorNotice message={error} clearError={() => setError(undefined)} />
-        )}
-        <form className="register-form" onSubmit={submit}>
-          <label htmlFor="register-email">Email</label>
-          <input
-            id="register-email"
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    return (
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="left"
+            className="page"
+        >
+            <Header />
+            <Box className="register-page">
+                <h1>Register</h1>
+                {error && (
+                    <ErrorNotice message={error} clearError={() => setError(undefined)} />
+                )}
+                <form className="register-form" onSubmit={submit}>
+                    <label htmlFor="register-email">Email</label>
+                    <input
+                        id="register-email"
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
-          <label htmlFor="register-password">Password</label>
-          <input
-            id="register-password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label htmlFor="register-verify-password">Verify password</label>
-          <input
-            type="password"
-            onChange={(e) => setPasswordCheck(e.target.value)}
-          />
+                    <label htmlFor="register-password">Password</label>
+                    <input
+                        id="register-password"
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <label htmlFor="register-verify-password">Verify password</label>
+                    <input
+                        type="password"
+                        onChange={(e) => setPasswordCheck(e.target.value)}
+                    />
 
-          <label htmlFor="register-display-name">Display name</label>
-          <input
-            id="register-display-name"
-            type="text"
-            onChange={(e) => setDisplayName(e.target.value)}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            size="small"
-          >
-            Go
+                    <label htmlFor="register-display-name">Display name</label>
+                    <input
+                        id="register-display-name"
+                        type="text"
+                        onChange={(e) => setDisplayName(e.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                    >
+                        Go
           </Button>
-        </form>
-      </Box>
-      <Footer />
-    </Grid>
-  );
+                </form>
+            </Box>
+            <Footer />
+        </Grid>
+    );
 }
